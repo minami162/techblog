c73ba01f7efb2646c452358c625fd265c134a2aa
Takumi <namitami162@gmail.com>
2024-12-27 19:48:52 +0900

    カード作成

--- app/page.js
+++ app/page.js
@@ -3,37 +3,46 @@ import { getBlogs } from '@/libs/client';
 
 import Header from './header/page';
 import PageTitle from './components/title/page';
+import './styles.css'; // スタイルを適用するためのCSSファイルを用意
 
 const BlogPage = async () => {
   // APIからデータを取得
   const blogs = await getBlogs();
-  
 
   return (
     <div>
-      <Header/>
-      <PageTitle title='ブログ一覧' title_en='blog' />
+      <Header />
+      <PageTitle title="ブログ一覧" title_en="blog" />
 
-     
-
-      <ul>
-        {/* データが存在する場合、リストを表示 */}
+      <div className="blog-container">
+        {/* データが存在する場合、カードを表示 */}
         {blogs && blogs.length > 0 ? (
           blogs.map((blog) => (
-            
-            <article>
-            <li key={blog.id}>
-              <h2>{blog.title}</h2> {/* タイトルを表示 */}
-              <p>{blog.body}</p> {/* コンテンツを表示 */}
-              
-              <p>{new Date(blog.date).toLocaleDateString()}</p> {/* 日付を表示 */}
-            </li>
-            </article>
+            <div key={blog.id} className="blog-card">
+              {/* サムネイル画像 */}
+              {blog.thumbnail && (
+                <img
+                  src={blog.thumbnail.url}
+                  alt={blog.title}
+                  className="blog-thumbnail"
+                />
+              )}
+              <div className="blog-content">
+                {/* 日付 */}
+                <p className="blog-date">
+                  {new Date(blog.date).toLocaleDateString()}
+                </p>
+                {/* タイトル */}
+                <h2 className="blog-title">{blog.title}</h2>
+                {/* 概要 */}
+                <p className="blog-summary">{blog.summary}</p>
+              </div>
+            </div>
           ))
         ) : (
-          <p>ブログデータがありません。</p> // データがない場合のメッセージ
+          <p>ブログデータがありません。</p>
         )}
-      </ul>
+      </div>
     </div>
   );
 };
