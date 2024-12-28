1db3d625afaa6051bba5f22c00fa1adea4dd6a30
Takumi <namitami162@gmail.com>
2024-12-28 12:02:20 +0900

    一時保存

--- app/category/page.js
+++ app/category/page.js
@@ -6,12 +6,10 @@ import PageTitle from '../components/title/page';
 import '../styles.css'; // スタイルを適用するためのCSSファイルを用意
 
 const BlogPage = async () => {
-  // APIからデータを取得
   const blogs = await getBlogs();
 
-  // カテゴリごとにブログを分類
   const categorizedBlogs = blogs.reduce((acc, blog) => {
-    const category = blog.category || 'その他'; // カテゴリがない場合は「その他」に分類
+    const category = blog.category || 'その他';
     if (!acc[category]) {
       acc[category] = [];
     }
@@ -25,20 +23,16 @@ const BlogPage = async () => {
       <PageTitle title="カテゴリー一覧" title_en="category" />
 
       <div className="blog-container">
-        {/* カテゴリごとにブログを表示 */}
         {Object.entries(categorizedBlogs).map(([category, blogs]) => (
           <div key={category} className="category-section">
-            {/* カテゴリ名をボックス風にデザイン */}
             <div className="category-title-box">
               <h3 className="category-title">{category}</h3>
             </div>
-
-            {/* カテゴリ内のブログ */}
             <div className="category-blogs">
               {blogs.map((blog) => (
                 <Link key={blog.id} href={`/techblog/${blog.id}`} passHref>
                   <div className="blog-card">
-                    {/* サムネイル画像 */}
+                    <span className="category-label">{category}</span>
                     {blog.thumbnail && (
                       <img
                         src={blog.thumbnail.url}
@@ -47,13 +41,10 @@ const BlogPage = async () => {
                       />
                     )}
                     <div className="blog-content">
-                      {/* 日付 */}
                       <p className="blog-date">
                         {new Date(blog.date).toLocaleDateString()}
                       </p>
-                      {/* タイトル */}
                       <h4 className="blog-title">{blog.title}</h4>
-                      {/* 概要 */}
                       <p className="blog-summary">{blog.summary}</p>
                     </div>
                   </div>
