d9b4452761f0cd58192fff7c51a812e3d1c7a926
Takumi <namitami162@gmail.com>
2024-12-28 22:08:26 +0900

    ヘッダーボタン最適化

--- app/components/header/page.js
+++ app/components/header/page.js
@@ -1,23 +1,45 @@
+'use client'; // クライアントコンポーネントとして実行
 import Link from 'next/link';
-import styles from './styles.module.css'; // CSSモジュールをインポート
+import { usePathname } from 'next/navigation'; // 現在のパスを取得
+import styles from './styles.module.css';
 
 export default function Header() {
+  const pathname = usePathname(); // 現在のパスを取得
+
   return (
     <>
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <Link href="/">
-          <button className={styles.button}>Top</button>
-        </Link>
-        <Link href="/news">
-          <button className={styles.button}>News</button>
-        </Link>
-        <Link href="/category">
-          <button className={styles.button}>Category</button>
-        </Link>
-      </nav>
-    </header>
-    <hr></hr>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <Link href="/">
+            <button
+              className={`${styles.button} ${
+                pathname === '/' ? styles.active : ''
+              }`}
+            >
+              Top
+            </button>
+          </Link>
+          <Link href="/news">
+            <button
+              className={`${styles.button} ${
+                pathname === '/news' ? styles.active : ''
+              }`}
+            >
+              News
+            </button>
+          </Link>
+          <Link href="/category">
+            <button
+              className={`${styles.button} ${
+                pathname === '/category' ? styles.active : ''
+              }`}
+            >
+              Category
+            </button>
+          </Link>
+        </nav>
+      </header>
+      <hr className={styles.hr} />
     </>
   );
 }
